1df044707319e528d3a8c099767c6c6b7823e28e
zinebnadak <zinebnadak04@outlook.com>
2025-09-27 09:47:27 +0300

    27.9..

--- #7Algorithms.py
+++ #7Algorithms.py
@@ -2,7 +2,7 @@
 # write an algorithm of how you´ll solve this problem
 # prime number is a number only divisible by 1 or itself
 
-#Ask user for input "n"
+#Ask user for input "n".
 #start a list to collect primenumbers up to n (included)
 #Two for loops: first to go trough all numbers from 1-n, one by one. Second will actually test all numbers from 1-n to see if the number has a another divider. If so it is not a primenumber and will not be added to list
 #print list of primenumbers as result
